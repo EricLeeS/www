68e0c4b14e66da43672b2b5fba76032b0b5775e9
lh <Administrator@PC-20140505HUYX>
2015-06-01 23:29:39 +0800

    example

--- blog/app/Http/Middleware/RedirectIfAuthenticated.php
+++ blog/app/Http/Middleware/RedirectIfAuthenticated.php
@@ -1,44 +1,53 @@
-<?php namespace App\Http\Middleware;
+<?php
+
+
+namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Contracts\Auth\Guard;
 use Illuminate\Http\RedirectResponse;
 
 class RedirectIfAuthenticated {
-
+	
 	/**
 	 * The Guard implementation.
 	 *
 	 * @var Guard
 	 */
 	protected $auth;
-
+	
 	/**
 	 * Create a new filter instance.
 	 *
-	 * @param  Guard  $auth
+	 * @param Guard $auth        	
 	 * @return void
 	 */
-	public function __construct(Guard $auth)
-	{
+	public function __construct(Guard $auth) {
 		$this->auth = $auth;
 	}
-
+	
 	/**
 	 * Handle an incoming request.
 	 *
-	 * @param  \Illuminate\Http\Request  $request
-	 * @param  \Closure  $next
+	 * @param \Illuminate\Http\Request $request        	
+	 * @param \Closure $next        	
 	 * @return mixed
 	 */
-	public function handle($request, Closure $next)
-	{
-		if ($this->auth->check())
-		{
-			return new RedirectResponse(url('/home'));
+	public function handle($request, Closure $next) {
+		// if ($this->auth->check())
+		// {
+		// return new RedirectResponse(url('/home'));
+		// }
+		
+		// return $next($request);
+		if ($this->auth->check ()) {
+			if (! Auth::user ()->is_admin) {
+				return new RedirectResponse ( url ( '/stu/home' ) );
+			} else {
+				return new RedirectResponse ( url ( '/admin' ) );
+			}
 		}
-
-		return $next($request);
+		
+		return $next ( $request );
 	}
-
 }
---
+++ blog/resources/views/test.blade.php
@@ -0,0 +1,16 @@
+@extends('app')
+
+
+@section('content')
+
+<div class="container">
+    {!! Form::open(['route' => 'test_upload', 'files' => true]) !!}
+        {!! Form::file('grade') !!}
+        {!! Form::submit('Submit') !!}
+    {!! Form::close() !!}
+
+</div>
+
+</body>
+</html>
+@stop
